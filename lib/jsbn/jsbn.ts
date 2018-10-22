62771643b855d189ae75524f19a05bbf9aa39439
lihai <lihai@iboxpay.com>
2018-10-22 17:23:54 +0800

    去掉 navigatior 和 window 等一些浏览器相关的变量

--- lib/jsbn/jsbn.ts
+++ lib/jsbn/jsbn.ts
@@ -13,7 +13,6 @@ let dbits;
 const canary = 0xdeadbeefcafe;
 const j_lm = ((canary & 0xffffff) == 0xefcafe);
 
-
 //#region
 const lowprimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997];
 const lplim = (1 << 26) / lowprimes[lowprimes.length - 1];
@@ -1849,16 +1848,19 @@ function am3(i:number, x:number, w:BigInteger, j:number, c:number, n:number) {
     return c;
 }
 
-if (j_lm && (navigator.appName == "Microsoft Internet Explorer")) {
-    BigInteger.prototype.am = am2;
-    dbits = 30;
-} else if (j_lm && (navigator.appName != "Netscape")) {
-    BigInteger.prototype.am = am1;
-    dbits = 26;
-} else { // Mozilla/Netscape seems to prefer am3
-    BigInteger.prototype.am = am3;
-    dbits = 28;
-}
+// if (j_lm && (navigator && navigator.appName == "Microsoft Internet Explorer")) {
+//     BigInteger.prototype.am = am2;
+//     dbits = 30;
+// } else if (j_lm && (navigator && navigator.appName != "Netscape")) {
+//     BigInteger.prototype.am = am1;
+//     dbits = 26;
+// } else { // Mozilla/Netscape seems to prefer am3
+    // BigInteger.prototype.am = am3;
+    // dbits = 28;
+// }
+
+BigInteger.prototype.am = am3;
+dbits = 28;
 
 BigInteger.prototype.DB = dbits;
 BigInteger.prototype.DM = ((1 << dbits) - 1);
